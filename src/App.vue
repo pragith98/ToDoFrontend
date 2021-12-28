274441041fe7026c4acb8efea710df9ed02cb529
pragith98 <lakshanxp@hotmail.com>
2021-12-28 16:26:40 +0530

    add router-view to app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
-  <!-- <router-view></router-view> -->
+
   <div>
     <app-navbar></app-navbar>
     <div class="container">
       <div class="row justify-content-center">
         <div class="mt-5 col-6 ">
           
-            <app-AddTask></app-AddTask>
+            <Router-view></Router-view>
           
         </div>
       </div>
@@ -17,12 +17,11 @@
 </template>
 
 <script>
-  import AddTask from './views/AddTask.vue'
   import navbar from './components/NavBar.vue'
+  
 
   export default{
     components:{
-      'app-AddTask':AddTask,
       'app-navbar':navbar
     }
   }
