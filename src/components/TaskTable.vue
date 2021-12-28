6a7de3bb83a506ef4c2dd13f2099dc376ae2208f
pragith98 <lakshanxp@hotmail.com>
2021-12-28 16:11:44 +0530

    create task table component

---
+++ src/components/TaskTable.vue
@@ -0,0 +1,15 @@
+<template>
+    <table class="table table-hover">
+        <thead>
+            <tr class="table-primary">
+                <th>1 row</th>
+                <th>1 row</th>
+                <th>1 row</th>
+            </tr>
+        </thead>
+        <tbody>
+            <td>adfdsf</td>
+            <td>adfdsf</td>
+        </tbody>
+    </table>
+</template>
