0f266128c097493d93769ce639f7c729fb5dc248
pragith98 <lakshanxp@hotmail.com>
2021-12-28 16:18:22 +0530

    import tasktable component to tasklist page

---
+++ src/views/TaskList.vue
@@ -0,0 +1,13 @@
+<template>
+    
+</template>
+
+<script>
+    import TaskTable from '../components/TaskTable.vue'
+
+    export default{
+        components:{
+            'app-TaskTable': TaskTable
+        }
+    }
+</script>
