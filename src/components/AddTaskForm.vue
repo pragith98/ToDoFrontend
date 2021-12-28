f7f74735e3502826a3a74141f03d0e9bbd77f4bf
pragith98 <lakshanxp@hotmail.com>
2021-12-28 15:08:38 +0530

    create task form

--- src/components/AddTaskForm.vue
+++ src/components/AddTaskForm.vue
@@ -1,22 +1,24 @@
 <template>
     <form>
         <fieldset>
-            <legend>Legend</legend>
-            <div class="form-group row">
-            <label for="staticEmail" class="col-sm-2 col-form-label">Email</label>
-            <div class="col-sm-10">
-                <input type="text" readonly="" class="form-control-plaintext" id="staticEmail" value="email@example.com">
-            </div>
-            </div>
+            <legend>Add Task</legend>
+            
             <div class="form-group">
-            <label for="exampleInputEmail1" class="form-label mt-4">Email address</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                <label class="form-label mt-4">New Task</label>
+                <textarea class="form-control" id="exampleTextarea" rows="3" placeholder="Enter your Task"></textarea>
+                <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
             </div>
-            <div class="form-group">
-            <label for="exampleInputPassword1" class="form-label mt-4">Password</label>
-            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+            
+            <div class="d-grid gap-2 mt-3">
+                <button class="btn btn-lg btn-primary" type="button">Save Task</button>
             </div>
+            
         </fieldset>
     </form>
 </template>
+
+<style scoped>
+    textarea {
+        resize: none;
+    }
+</style>
