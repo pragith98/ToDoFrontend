e061daab77c4c3522f49866599b4a4b54a0dc946
pragith98 <lakshanxp@hotmail.com>
2021-12-28 14:12:56 +0530

    create route to addtask page and change index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,12 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import AddTask from '../views/AddTask'
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  {path: '/AddTask', component: AddTask},
+  {path: '/', component: AddTask}
 ]
 
 const router = new VueRouter({
@@ -25,3 +14,20 @@ const router = new VueRouter({
 })
 
 export default router
+
+
+
+
+// {
+//   path: '/',
+//   name: 'Home',
+//   component: Home
+// },
+// {
+//   path: '/about',
+//   name: 'About',
+//   // route level code-splitting
+//   // this generates a separate chunk (about.[hash].js) for this route
+//   // which is lazy-loaded when the route is visited.
+//   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+// }
