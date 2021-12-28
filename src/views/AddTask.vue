a217c1c0f40ffcb3103d87a45fc5b51d281d965f
pragith98 <lakshanxp@hotmail.com>
2021-12-28 13:49:31 +0530

    import addTaskForm component to AddTask page

---
+++ src/views/AddTask.vue
@@ -0,0 +1,15 @@
+<template>
+    <app-AddTaskForm></app-AddTaskForm>
+    
+</template>
+
+<script>
+    import AddTaskForm from '../components/AddTaskForm.vue'
+
+    export default{
+        components:{
+            "app-AddTaskForm":AddTaskForm
+        }
+    }
+
+</script>
