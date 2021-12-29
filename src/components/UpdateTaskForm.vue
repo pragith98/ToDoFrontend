292d5415fb356bfd8f233b4eefdee51fce8e520d
pragith98 <lakshanxp@hotmail.com>
2021-12-29 23:46:58 +0530

    add validation to updatetask form

--- src/components/UpdateTaskForm.vue
+++ src/components/UpdateTaskForm.vue
@@ -1,25 +1,48 @@
 <template>
-    <form>
-        <fieldset>
-            <h2>Update Task</h2>
-            
-            <div class="form-group">
-                <label class="form-label mt-4">Update Task</label>
-                <textarea class="form-control" id="exampleTextarea" rows="3" placeholder="Enter your Task"></textarea>
-                <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-            </div>
-            
-            <div class="mt-3">
-                <button class="btn btn-lg btn-success" type="button">Update Task</button>
-                <router-link to='/TaskList' class="btn btn-lg btn-outline-secondary mx-2">Cancel</router-link>
-            </div>
-            
-        </fieldset>
-    </form>
+    <ValidationObserver v-slot="{handleSubmit}">
+        <form @submit.prevent="handleSubmit(onSubmit)">
+            <fieldset>
+                <h2>Update Task</h2>
+                
+                <div class="form-group">
+                    <label class="form-label mt-4">Update Task</label>
+                    <ValidationProvider name="Task" rules="required" v-slot="{errors}">
+                        <textarea v-model="task" class="form-control" id="exampleTextarea" rows="3" placeholder="Enter your Task"></textarea>
+                        <small class="form-text">{{errors[0]}}</small>
+                    </ValidationProvider>
+                </div>
+                
+                <div class="mt-3">
+                    <button class="btn btn-lg btn-success" type="submit">Update Task</button>
+                    <router-link to='/TaskList' class="btn btn-lg btn-outline-secondary mx-2">Cancel</router-link>
+                </div>
+                
+            </fieldset>
+        </form>
+    </ValidationObserver>
 </template>
+
+<script>
+    export default{
+        data:()=>({
+            task:""
+        }),
+        
+        methods:{
+            onSubmit(){
+                console.log(this.task)
+            }
+        }
+    }
+</script>
+
 
 <style scoped>
     textarea {
         resize: none;
+    }
+
+    small{
+        color: red;
     }
 </style>
