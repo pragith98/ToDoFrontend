6d91755181b0defc2687b3e6db512f1bf2910245
pragith98 <lakshanxp@hotmail.com>
2021-12-29 14:08:51 +0530

    create updatetask form and update task view

---
+++ src/components/UpdateTaskForm.vue
@@ -0,0 +1,26 @@
+<template>
+    <form>
+        <fieldset>
+            <h2>Add Task</h2>
+            
+            <div class="form-group">
+                <label class="form-label mt-4">New Task</label>
+                <textarea class="form-control" id="exampleTextarea" rows="3" placeholder="Enter your Task"></textarea>
+                <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+            </div>
+            
+            <div class="mt-3">
+                <!-- <button class="btn btn-lg btn-primary" type="button">Save Task</button> -->
+                <button class="btn btn-lg btn-primary" type="button">Save Task</button>
+                <router-link to='/TaskList' class="btn btn-lg btn-outline-secondary mx-2">Cancel</router-link>
+            </div>
+            
+        </fieldset>
+    </form>
+</template>
+
+<style scoped>
+    textarea {
+        resize: none;
+    }
+</style>
---
+++ src/views/UpdateTask.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="row justify-content-center">
+        <div class="col-lg-6 ">
+            <app-AddTaskForm></app-AddTaskForm>
+        </div>
+    </div>
+</template>
+
+<script>
+    import AddTaskForm from '../components/AddTaskForm.vue'
+
+    export default{
+        components:{
+            "app-AddTaskForm":AddTaskForm
+        }
+    }
+
+</script>
