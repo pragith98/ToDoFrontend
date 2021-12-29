16499a43ce46ebc4c45f3dcbf6ef0d2ea9efaae4
pragith98 <lakshanxp@hotmail.com>
2021-12-30 00:29:42 +0530

    install axios and import to mainjs

--- src/main.js
+++ src/main.js
@@ -5,8 +5,13 @@ import router from './router'
 import { ValidationProvider } from 'vee-validate/dist/vee-validate.full.esm';
 import {ValidationObserver} from 'vee-validate';
 
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
 Vue.component('ValidationProvider',ValidationProvider)
 Vue.component('ValidationObserver',ValidationObserver)
+
+Vue.use(VueAxios, axios)
 
 Vue.config.productionTip = false
 
