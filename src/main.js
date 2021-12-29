17d46b480224a7085d2e386780734dde2ebd91a8
pragith98 <lakshanxp@hotmail.com>
2021-12-29 22:48:32 +0530

    import and register vee-validate in main.js

--- src/main.js
+++ src/main.js
@@ -2,6 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import { ValidationProvider } from 'vee-validate/dist/vee-validate.full.esm';
+import {ValidationObserver} from 'vee-validate';
+
+Vue.component('ValidationProvider',ValidationProvider)
+Vue.component('ValidationObserver',ValidationObserver)
+
 Vue.config.productionTip = false
 
 new Vue({
