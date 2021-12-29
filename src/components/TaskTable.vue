26c06dd8da9a63efdc434ff85be4099c217f98ff
pragith98 <lakshanxp@hotmail.com>
2021-12-29 14:13:40 +0530

    add router-link to update

--- src/components/TaskTable.vue
+++ src/components/TaskTable.vue
@@ -22,7 +22,8 @@
                     <td align="right">
                         <!-- <button type="button" class="btn btn-outline-warning btn-sm">Completed</button> -->
                         <button type="button" class="btn btn-outline-success btn-sm" style="width:80px">New</button>
-                        <button type="button" class="btn btn-secondary mx-2 btn-sm">Update</button>
+                        <!-- <button type="button" class="btn btn-secondary mx-2 btn-sm">Update</button> -->
+                        <router-link to='/UpdateTask' class="btn btn-secondary mx-2 btn-sm">Update</router-link>
                         <button type="button" class="btn btn-danger btn-sm">Delete</button>
                     </td>
                 </tr>
