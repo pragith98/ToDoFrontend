4bba77af0bece18689fc52d8634b847fab9cabca
pragith98 <lakshanxp@hotmail.com>
2021-12-29 13:41:08 +0530

    create task table

--- src/components/TaskTable.vue
+++ src/components/TaskTable.vue
@@ -1,12 +1,17 @@
 <template>
     <div>
-        <legend>Add Task</legend>
+        <div>
+           <h2 class="float-start">Task List</h2> 
+           <button type="button" class="btn btn-primary float-end mb-2">Add New</button>
+            
+        </div>
+        
         <table class="table table-hover">
             <thead>
                 <tr class="table-primary">
                     <th>ID</th>
                     <th>Task</th>
-                    <th></th>
+                    <th style="width:55%"></th>
                 </tr>
             </thead>
             <tbody>
@@ -14,11 +19,21 @@
                 <tr>
                     <td>1</td>
                     <td>Go To Schoolsdfsdf sdfasdfsd</td>
-                    <td>
-                        <button type="button" class="btn btn-primary">Primary</button>
-                        <button type="button" class="btn btn-primary ml-2">Primary</button>
-                        <button type="button" class="btn btn-primary">Primary</button>
-                    
+                    <td align="right">
+                        <!-- <button type="button" class="btn btn-outline-warning btn-sm">Completed</button> -->
+                        <button type="button" class="btn btn-outline-success btn-sm" style="width:80px">New</button>
+                        <button type="button" class="btn btn-secondary mx-2 btn-sm">Update</button>
+                        <button type="button" class="btn btn-danger btn-sm">Delete</button>
+                    </td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>Go To Schoolsdfsdf sdfasdfsd</td>
+                    <td align="right">
+                        <button type="button" class="btn btn-outline-warning btn-sm">Completed</button>
+                        <!-- <button type="button" class="btn btn-outline-success btn-sm" style="width:80px">New</button> -->
+                        <button type="button" class="btn btn-secondary mx-2 btn-sm">Update</button>
+                        <button type="button" class="btn btn-danger btn-sm">Delete</button>
                     </td>
                 </tr>
                 
