720e4279dc54e5bda90e69a8de43be200e716a19
pragith98 <lakshanxp@hotmail.com>
2021-12-31 01:25:31 +0530

    create delete task with axios

--- src/components/TaskTable.vue
+++ src/components/TaskTable.vue
@@ -23,7 +23,7 @@
                         <button type="button" class="btn btn-outline-warning btn-sm">Completed</button>
                         <!-- <button type="button" class="btn btn-outline-success btn-sm" style="width:80px">New</button> -->
                         <router-link to='/UpdateTask' class="btn btn-success mx-2 btn-sm">Update</router-link>
-                        <button type="button" class="btn btn-danger btn-sm">Delete</button>
+                        <button type="button" class="btn btn-danger btn-sm" @click="deleteTask(task.id)" >Delete</button>
                     </td>
                 </tr>
                 
@@ -43,7 +43,31 @@
 
         created(){
             this.axios.get("http://localhost:8001/api/allTask").then(Response=>(this.tasks=Response.data.allTask))
+        },
+
+        methods:{
+            deleteTask(id){
+                var taskval=this.tasks;
+
+                console.log(id);
+                this.axios.delete("http://localhost:8001/api/deleteTask/"+id).then(function(Response){
+                    console.log(Response);
+                    
+                    var position=taskval.findIndex(function(element){
+                        return element.id==id;
+                    });
+
+                    taskval.splice(position,1);
+                });
+
+                
+
+                
+                
+
+            }
         }
+        
     }
 
 </script>
